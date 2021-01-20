829ff8c61faf9dc2d90ea51cb8b5c0420d37f693
Jaysree <js@ramaka.com>
2021-01-20 18:52:56 +0530

    new added

---
+++ exercises/bmi.ts
@@ -0,0 +1,15 @@
+// enum values {
+//   weight = 56,
+//   height = 6,
+// }
+function bmi(weight: number, height: number): string {
+  let bmi = weight / Math.sqrt(height);
+  return bmi <= 18.5
+    ? "Under weight"
+    : bmi <= 25.0
+    ? "Normal"
+    : bmi <= 30.0
+    ? "Overweight"
+    : "Obese";
+}
+console.log(bmi(80, 1.8));
