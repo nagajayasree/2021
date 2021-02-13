18e01bccc3954c76248a98628720f16187a6e01d
Jaysree <js@ramaka.com>
2021-02-13 22:15:19 +0530

    added changes quizApp

--- src/practice/Quiz/quiz.tsx
+++ src/practice/Quiz/quiz.tsx
@@ -24,24 +24,55 @@ class Quiz extends React.Component<QuizProps, QuizState> {
           ],
           answer: "peacock",
         },
+        {
+          question: "Simple Math 2 + 3 = ?",
+          options: [{ op1: "8", op2: "10", op3: "5", op4: "15" }],
+          answer: "5",
+        },
       ],
-
       score: 0,
     };
   }
-
-  onButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    let selected = e.currentTarget.innerText;
-    let ans = this.state.quesArr.map((i) => {
-      return i.answer;
-    });
-    if (selected === ans.toString()) {
+  onButtonClick = (opt: any) => {
+    let selected = opt;
+    console.log(selected);
+    let ans = this.state.quesArr
+      .filter((i) => i.answer === selected)
+      .map((i) => {
+        // console.log("answer" + ":" + i.answer);
+        return i.answer;
+      });
+    console.log(ans.toString());
+    if (selected == ans.toString()) {
+      console.log("correct ans");
       this.setState({ score: this.state.score + 1 });
-      console.log("correct");
+    } else {
+      alert("wrong ans");
+      console.log("wrong answer");
     }
-    // console.log(selected);
-    // console.log(typeof selected);
+    // let getAns = this.state.quesArr.map((item, index) => {
+    //   return item.answer;
+    // });
+    // console.log(ans.toString());
+    // let ans = this.state.quesArr.map((i) => {
+    //   return i.answer;
+    // });
+    // let ans = this.state.quesArr.forEach((item, index) => {
+    //   if (item.answer === selected) {
+    //     console.log("correct");
+    //   }
+    //   return item.answer, index;
+    // });
+    // if (selected === ans) {
+    //   console.log("correct");
+    //   this.setState({ score: this.state.score + 1 });
+    // } else {
+    //   console.log("wrong");
+    // }
+    // console.log(ans);
     // console.log(typeof ans);
+    // console.log(ans[0].toString());
+    // // console.log(typeof selected);
     // console.log(ans.toString());
   };
 
@@ -50,29 +81,36 @@ class Quiz extends React.Component<QuizProps, QuizState> {
       <>
         {this.props.title}
         <div>
-          <p>
-            Q:
-            {this.state.quesArr.map((i) => {
-              return i.question;
-            })}
-          </p>
-          Options:
           {this.state.quesArr.map((i) => {
-            return i.options.map((o) => {
-              return (
+            return (
+              <>
                 <div>
-                  <button onClick={this.onButtonClick}>{o.op1}</button>
-                  <button onClick={this.onButtonClick}>{o.op2}</button>
-                  <button onClick={this.onButtonClick}>{o.op3}</button>
-                  <button onClick={this.onButtonClick}>{o.op4}</button>
+                  <p>Q:{i.question}</p>
+                  <p>
+                    {i.options.map((opt) => {
+                      return (
+                        <div>
+                          <button onClick={() => this.onButtonClick(opt.op1)}>
+                            {opt.op1}
+                          </button>
+                          <button onClick={() => this.onButtonClick(opt.op2)}>
+                            {opt.op2}
+                          </button>
+                          <button onClick={() => this.onButtonClick(opt.op3)}>
+                            {opt.op3}
+                          </button>
+                          <button onClick={() => this.onButtonClick(opt.op4)}>
+                            {opt.op4}
+                          </button>
+                        </div>
+                      );
+                    })}
+                  </p>
                 </div>
-              );
-            });
+              </>
+            );
           })}
-          <p>
-            Score:
-            {this.state.score}
-          </p>
+          <p>Score:{this.state.score}</p>
         </div>
       </>
     );
